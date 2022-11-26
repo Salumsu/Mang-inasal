7c0f9ae3a8303cae436e31e21dd308ab808da0ad
Salumsu <75105743+Salumsu@users.noreply.github.com>
2022-11-26 15:25:49 +0800

    Delete app directory

--- app/lib/get_param.php
+++
@@ -1,4 +0,0 @@
-<?php
-    function get_param(string $name, string $default = '') {
-        return isset($_REQUEST[$name]) ? $_REQUEST[$name] : $default;
-    }
--- app/misc/js/loginPopup.js
+++
@@ -1,8 +0,0 @@
-const notAuths = document.querySelectorAll(".not-auth");
-const loginPopup = document.querySelector("#login-popup");
-
-notAuths.forEach((notAuth) => {
-  notAuth.addEventListener("click", () => {
-    loginPopup.classList.toggle("active");
-  });
-});
